2c42ccc92de43a4e91eaa3fa79bd63d72527bcb5
Yosiris Mariñez <yosiris0510@gmail.com>
2021-10-23 19:26:23 +0200

    save and get friends from localStorage

--- src/services/friends.ts
+++ src/services/friends.ts
@@ -1,9 +1,21 @@
-const friends: string[] = ["Pablo", "Luis", "Pedro", "Lucas"];
-
 export function getFriends(): string[] {
-  return friends;
+  return getFriendsFromLocalStorage();
 }
 
 export function createFriend(name: string) {
+  const friends = getFriendsFromLocalStorage();
   friends.push(name);
+  saveFriendsInLocalStorage(friends);
+}
+
+function getFriendsFromLocalStorage(): string[] {
+  const friendsJson = localStorage.getItem("friends");
+  if (friendsJson === null) {
+    return [];
+  }
+  return JSON.parse(friendsJson);
+}
+
+function saveFriendsInLocalStorage(friends: string[]): void {
+  localStorage.setItem("friends", JSON.stringify(friends));
 }
