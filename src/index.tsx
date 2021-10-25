a0075e550c3467b360b4af66657e6cf50b314c25
Yosiris Mariñez <yosiris0510@gmail.com>
2021-10-25 17:46:48 +0200

    delete report web vitals

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
@@ -11,5 +10,3 @@ ReactDOM.render(
   </HashRouter>,
   document.getElementById("root")
 );
-
-reportWebVitals();
