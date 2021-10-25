96c0ba62576d2982e39a39c7dc380a8c0b34dc81
Yosiris Mariñez <yosiris0510@gmail.com>
2021-10-25 20:11:19 +0200

    fix app render test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders prints welcome message", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const welcomeMessage = screen.getByText(/Welcome to SharedExpenses/i);
+  expect(welcomeMessage).toBeInTheDocument();
 });
